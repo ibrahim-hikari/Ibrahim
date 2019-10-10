f941facdaf6f20de9f450fa8d8ec7d298238ef2c
Ibrahim <ajarmeh1997@gmail.com>
2019-10-10 10:42:16 +0300

    blank prob fixed

--- js/test.js
+++ js/test.js
@@ -22,7 +22,7 @@ alert('good to have you here ' + greetings);
 //     userResponse = userResponse.toLowerCase();
 //     var yesRespnse = currentQuestion[1];
 //     var noResponse = currentQuestion[2];
- 
+
 
 //     while (userResponse !== 'yes' && userResponse !== 'y' && userResponse !== 'no' && userResponse !== 'n') {
 //         userResponse = prompt('Sorry, it is yes or no question!');}
@@ -36,15 +36,18 @@ alert('good to have you here ' + greetings);
 //             case 'n':
 //                 alert(noResponse);
 //                 break;
-        
+
 //     }
 
 // }
 
 ///////////////////////// change #1 
-function askQuestion( questions ,yesResponse , noResponse){
+function askQuestion(questions, yesResponse, noResponse) {
     var response = prompt(questions)
     response = response.toUpperCase();
+    while (response !== 'yes' && response !== 'y' && response !== 'no' && response !== 'n') {
+        response = prompt('Sorry, it is yes or no question!');
+    }
     if (response == 'YES' || response == 'Y') {
         alert(yesResponse);
         score++;
@@ -54,80 +57,80 @@ function askQuestion( questions ,yesResponse , noResponse){
         }
     }
 }
-  askQuestion('Do you know me?', 'good for you, let us find more ', 'go ahead and find out who I am')
-  askQuestion('do you know where I\'m from?', 'great, we should see eachother when we have the chance', 'I\'m from Tafila, it\'s a great place, you should visit me, we\'ll have fun togather')
-  askQuestion('do you know when is my birthday?', 'awesome, I\'m making a party you shhould come', 'It in 15 jan, don\'t forget to git me a present :p')
-  askQuestion('Do you know what kind of movies do I like?', 'great, we should go and see one this weekend ', 'I love the mysterous one like SherlockHolmes movie')
-  askQuestion('do you know where I go when I\'m little upset', 'please don\'t tell anyone', 'of course, because no one should know')
+askQuestion('Do you know me?', 'good for you, let us find more ', 'go ahead and find out who I am')
+askQuestion('do you know where I\'m from?', 'great, we should see eachother when we have the chance', 'I\'m from Tafila, it\'s a great place, you should visit me, we\'ll have fun togather')
+askQuestion('do you know when is my birthday?', 'awesome, I\'m making a party you shhould come', 'It in 15 jan, don\'t forget to git me a present :p')
+askQuestion('Do you know what kind of movies do I like?', 'great, we should go and see one this weekend ', 'I love the mysterous one like SherlockHolmes movie')
+askQuestion('do you know where I go when I\'m little upset', 'please don\'t tell anyone', 'of course, because no one should know')
 
-  alert('thank you for your time ' + greetings + ' Wish you enjoy the webpage');
-  
-
+alert('thank you for your time ' + greetings + ' Wish you enjoy the webpage');
 
 
 
-function guess(){
+
+
+function guess() {
 
     var trailNum = 4;
     var trailQuestion = [
         ['What is my favourite number'],
-    ['ops, again!'],
-    ['I\'m sorry, try again'],
-    ['try harder'],
-];
-var ansewr = prompt(trailQuestion[0]);
+        ['ops, again!'],
+        ['I\'m sorry, try again'],
+        ['try harder'],
+    ];
+    var ansewr = prompt(trailQuestion[0]);
 
-for (var j = 1; j < trailNum; j++) {
-    
-    if (ansewr > 7) {
-        var currentTrail = trailQuestion[j];
-        var currentPrompt = currentTrail[0];
-        ansewr = prompt(currentPrompt + ' your ansewr is too high ');
-    } else if (ansewr < 7) {
-        var currentTrail = trailQuestion[j];
-        var currentPrompt = currentTrail[0];
-        ansewr = prompt(currentPrompt + ' your ansewr is too low');
-    } else if (ansewr == 7) {
-        alert('good job you got it');
-        j = trailNum;
-        score++;
+    for (var j = 1; j < trailNum; j++) {
+
+        if (ansewr > 7) {
+            var currentTrail = trailQuestion[j];
+            var currentPrompt = currentTrail[0];
+            ansewr = prompt(currentPrompt + ' your ansewr is too high ');
+        } else if (ansewr < 7) {
+            var currentTrail = trailQuestion[j];
+            var currentPrompt = currentTrail[0];
+            ansewr = prompt(currentPrompt + ' your ansewr is too low');
+        } else if (ansewr == 7) {
+            alert('good job you got it');
+            j = trailNum;
+            score++;
+        }
+
     }
 
-}
-
-return(ansewr)
+    return (ansewr)
 }
 
 guess();
 
-function subject(){
+function subject() {
 
     var attempt = 5;
     var material = ['vibrations', 'automatic-control', 'refrigeration-systems', 'heating-systems', 'thermodynamics', 'dynamics'];
-var repeat = ['sorry, try again', 'not even close', 'try harder'];
+    var repeat = ['sorry, try again', 'not even close', 'try harder', 'again!', 'and again please', 'last chance'];
 
-var question = prompt('What was my favourite subject back at the university');
+    var question = prompt('What was my favourite subject back at the university');
 
-for (var k = 0; k < attempt; k++) {
-    if (question !== material[k]) {
-        question = prompt(repeat[k])
-        for (var l = 0; l < attempt; l++) {
-            
-            if (question == material[l]) {
-                alert('WOW, you know me well');
-                l = attempt;
-                k = attempt;
-                score++;
+    for (var k = 0; k < attempt; k++) {
+        if (question !== material[k]) {
+            question = prompt(repeat[k])
+            for (var l = 0; l < attempt; l++) {
+
+                if (question == material[l]) {
+                    alert('WOW, you know me well');
+                    l = attempt;
+                    k = attempt;
+                    score++;
+                }
             }
+        } else {
+            alert('WOW, you know me well');
+            k = attempt;
+            score++;
         }
-    } else {
-        alert('WOW, you know me well');
-        k = attempt;
-        score++;
+
     }
-    
-}
-return(question);
+    return (question);
 }
 
 subject();
