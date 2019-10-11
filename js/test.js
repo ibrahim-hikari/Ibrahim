fde7a7a800c620821a14ead631da08db0b232836
Ibrahim <ajarmeh1997@gmail.com>
2019-10-11 18:09:35 +0300

    final edit on my Aboutme

--- js/test.js
+++ js/test.js
@@ -2,51 +2,14 @@
 
 var score = 0;
 
-var greetings = prompt('hello there, what\'s your name?');
-alert('good to have you here ' + greetings);
 
-// var qusetionLength = 5;
-// var questions = [
-//     ['Do you know me?', 'good for you, let us find more ', 'go ahead and find out who I am'], //Q1
-//     ['do you know where I\'m from?', 'great, we should see eachother when we have the chance', 'I\'m from Tafila, it\'s a great place, you should visit me, we\'ll have fun togather'], //Q2
-//     ['do you know when is my birthday?', 'awesome, I\'m making a party you shhould come', 'It in 15 jan, don\'t forget to git me a present :p'], //Q3
-//     ['Do you know what kind of movies do I like?', 'great, we should go and see one this weekend ', 'I love the mysterous one like SherlockHolmes movie'], //Q4
-//     ['do you know where I go when I\'m little upset', 'please don\'t tell anyone', 'of course, because no one should know'], //Q5
-// ];
-
-// for (var i = 0; i < qusetionLength; i++) {
-
-//     var currentQuestion = questions[i];
-//     var currentPrompt = currentQuestion[0];
-//     var userResponse = prompt(currentPrompt);
-//     userResponse = userResponse.toLowerCase();
-//     var yesRespnse = currentQuestion[1];
-//     var noResponse = currentQuestion[2];
-
-
-//     while (userResponse !== 'yes' && userResponse !== 'y' && userResponse !== 'no' && userResponse !== 'n') {
-//         userResponse = prompt('Sorry, it is yes or no question!');}
-//         switch (userResponse) {
-//             case 'yes':
-//             case 'y':
-//                 alert(yesRespnse);
-//                 score++;
-//                 break;
-//             case 'no':
-//             case 'n':
-//                 alert(noResponse);
-//                 break;
-
-//     }
-
-// }
-
-///////////////////////// change #1 
 function askQuestion(questions, yesResponse, noResponse) {
     var response = prompt(questions)
     response = response.toUpperCase();
-    while (response !== 'yes' && response !== 'y' && response !== 'no' && response !== 'n') {
+    while (response !== 'YES' && response !== 'Y' && response !== 'NO' && response !== 'N') {
         response = prompt('Sorry, it is yes or no question!');
+        response = response.toUpperCase();
+
     }
     if (response == 'YES' || response == 'Y') {
         alert(yesResponse);
@@ -57,16 +20,11 @@ function askQuestion(questions, yesResponse, noResponse) {
         }
     }
 }
-askQuestion('Do you know me?', 'good for you, let us find more ', 'go ahead and find out who I am')
-askQuestion('do you know where I\'m from?', 'great, we should see eachother when we have the chance', 'I\'m from Tafila, it\'s a great place, you should visit me, we\'ll have fun togather')
-askQuestion('do you know when is my birthday?', 'awesome, I\'m making a party you shhould come', 'It in 15 jan, don\'t forget to git me a present :p')
-askQuestion('Do you know what kind of movies do I like?', 'great, we should go and see one this weekend ', 'I love the mysterous one like SherlockHolmes movie')
-askQuestion('do you know where I go when I\'m little upset', 'please don\'t tell anyone', 'of course, because no one should know')
-
-alert('thank you for your time ' + greetings + ' Wish you enjoy the webpage');
-
-
-
+askQuestion('Do you know me?', 'good for you, let us find more ', 'go ahead and find out who I am');
+askQuestion('do you know where I\'m from?', 'great, we should see eachother when we have the chance', 'I\'m from Tafila, it\'s a great place, you should visit me, we\'ll have fun togather');
+askQuestion('do you know when is my birthday?', 'awesome, I\'m making a party you shhould come', 'It in 15 jan, don\'t forget to git me a present :p');
+askQuestion('Do you know what kind of movies do I like?', 'great, we should go and see one this weekend ', 'I love the mysterous one like SherlockHolmes movie');
+askQuestion('do you know where I go when I\'m little upset', 'please don\'t tell anyone', 'of course, because no one should know');
 
 
 function guess() {
@@ -79,6 +37,10 @@ function guess() {
         ['try harder'],
     ];
     var ansewr = prompt(trailQuestion[0]);
+    parseInt(ansewr);
+    while (isNaN(ansewr)) {
+        ansewr = prompt('please enter a number!!')
+    }
 
     for (var j = 1; j < trailNum; j++) {
 
@@ -86,10 +48,18 @@ function guess() {
             var currentTrail = trailQuestion[j];
             var currentPrompt = currentTrail[0];
             ansewr = prompt(currentPrompt + ' your ansewr is too high ');
+            parseInt(ansewr);
+            while (isNaN(ansewr)) {
+                ansewr = prompt('please enter a number!!')
+            }
         } else if (ansewr < 7) {
             var currentTrail = trailQuestion[j];
             var currentPrompt = currentTrail[0];
             ansewr = prompt(currentPrompt + ' your ansewr is too low');
+            parseInt(ansewr);
+            while (isNaN(ansewr)) {
+                ansewr = prompt('please enter a number!!')
+            }
         } else if (ansewr == 7) {
             alert('good job you got it');
             j = trailNum;
@@ -133,6 +103,15 @@ function subject() {
     return (question);
 }
 
+
 subject();
 
 alert('your score at this demo website is ' + score + ' mark');
+
+function yesFeedback() {
+    alert('Thank you, glad to know that')
+}
+
+function noFeedback() {
+    alert('oh, sorry about that hope you like my next work')
+}
